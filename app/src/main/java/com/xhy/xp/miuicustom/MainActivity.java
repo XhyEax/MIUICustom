73daf863042affbf70436e2452f48ddbb4fce5ae
XhyEax <xhyeax@qq.com>
2021-10-02 17:12:06 +0800

    simplify and add release.apk

--- app/src/main/java/com/xhy/xp/miuicustom/MainActivity.java
+++ app/src/main/java/com/xhy/xp/miuicustom/MainActivity.java
@@ -1,10 +1,9 @@
 package com.xhy.xp.miuicustom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
