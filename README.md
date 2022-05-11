ae6a1f488ef2db6f8718fcb4d43ce86ebbdaf032
XhyEax <xhyeax@qq.com>
2022-05-11 18:34:18 +0800

    update gradle and adapt 5.6.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MIUICustom
 MIUI 12 Custom Module (Xposed)
 ## APP Version Tested
-Security Center: `5.4.0`
+Security Center: `5.4.0`、`5.6.0`
 ## SecurityCenterHook
 [miui-countdowntimer-bypass](https://blog.xhyeax.com/2021/10/02/miui-countdowntimer-bypass/)
--- app/src/main/java/com/xhy/xp/miuicustom/dispatch/SecurityCenterHook.java
+++ app/src/main/java/com/xhy/xp/miuicustom/dispatch/SecurityCenterHook.java
@@ -6,18 +6,38 @@ import java.lang.reflect.Method;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class SecurityCenterHook {
-    public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
         // miui-countdowntimer-bypass
-        findAndHookMethod("com.miui.permcenter.privacymanager.d", lpparam.classLoader, "c", new XC_MethodReplacement() {
-            @Override
-            protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                Method mtdA = param.thisObject.getClass().getDeclaredMethod("a", boolean.class);
-                mtdA.invoke(param.thisObject, true);
-                return null;
-            }
-        });
+
+        // 5.4.0
+        Method mtd_SpecialPermissionIntercept_540 = XposedHelpers.findMethodExactIfExists("com.miui.permcenter.privacymanager.d", lpparam.classLoader, "c");
+        if (mtd_SpecialPermissionIntercept_540 != null) {
+            XposedBridge.hookMethod(mtd_SpecialPermissionIntercept_540, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    Method mtdConfirm = param.thisObject.getClass().getDeclaredMethod("a", boolean.class);
+                    mtdConfirm.invoke(param.thisObject, true);
+                    return null;
+                }
+            });
+        }
+
+        // 5.6.0
+        Method mtd_SpecialPermissionIntercept_560 = XposedHelpers.findMethodExactIfExists("com.miui.permcenter.privacymanager.h", lpparam.classLoader, "n");
+        if (mtd_SpecialPermissionIntercept_560 != null) {
+            XposedBridge.hookMethod(mtd_SpecialPermissionIntercept_560, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    Method mtdConfirm = param.thisObject.getClass().getDeclaredMethod("d", boolean.class);
+                    mtdConfirm.invoke(param.thisObject, true);
+                    return null;
+                }
+            });
+        }
     }
 }
